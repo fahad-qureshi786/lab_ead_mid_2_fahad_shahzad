42c6faeb961295427fdd94d959f35abdbfcbd98f
fahad-qureshi786 <mfahad.bses19@iba-suk.edu.pk>
2022-05-24 15:39:58 +0500

    create user interface

---
+++ server/model/users.js
@@ -0,0 +1,30 @@
+//defining schema
+const mongoose=require('mongoose');
+const userSchema= new mongoose.Schema({
+    fullName:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    phone:{
+        type:String,
+        required:true,
+    },
+    country:{
+        type:String,
+        required:true,
+    },
+    state:{
+        type:String,
+        required:true
+    },
+    zipCode:{
+        type:String,
+        required:true
+    }
+});
+
+module.exports=mongoose.model("User", userSchema);
